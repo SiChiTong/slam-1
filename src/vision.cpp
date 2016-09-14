b7156198385817ec134181c84df64ce1153b3ddd
Chris Choi <chutsu@gmail.com>
2016-09-14 00:04:16 -0400

    Retab vision module

--- src/vision.cpp
+++ src/vision.cpp
@@ -3,9 +3,9 @@
 
 Vision::Vision(void)
 {
-	cv::initModule_nonfree();
+    cv::initModule_nonfree();
 
-	this->initialized = false;
+    this->initialized = false;
 
     this->capture_index = 0;
     this->image_width = 0;
@@ -40,104 +40,104 @@ int Vision::initCamera(void)
 
 int Vision::initFeatureDetector(std::string type)
 {
-	this->fdetector = cv::FeatureDetector::create(type);
+    this->fdetector = cv::FeatureDetector::create(type);
     LOG_INFO("feature detector initialized!");
-	return 0;
+    return 0;
 }
 
 int Vision::initFeatureExtractor(std::string type)
 {
-	this->fextractor = cv::DescriptorExtractor::create(type);
+    this->fextractor = cv::DescriptorExtractor::create(type);
     LOG_INFO("feature extractor initialized!");
-	return 0;
+    return 0;
 }
 
 int Vision::initFeatureMatcher(std::string type)
 {
-	this->fmatcher = cv::DescriptorMatcher::create(type);
+    this->fmatcher = cv::DescriptorMatcher::create(type);
     LOG_INFO("feature matcher initialized!");
-	return 0;
+    return 0;
 }
 
 int Vision::detectFeatures(cv::Mat &image, std::vector<cv::KeyPoint> &key_pts)
 {
-	this->fdetector->detect(image, key_pts);
-	cv::drawKeypoints(
-		image,
-		key_pts,
-		image,
-		cv::Scalar::all(-1),
-		cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS
-	);
+    this->fdetector->detect(image, key_pts);
+    cv::drawKeypoints(
+        image,
+        key_pts,
+        image,
+        cv::Scalar::all(-1),
+        cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS
+    );
 
-	return 0;
+    return 0;
 }
 
 int Vision::extractFeatures(
-	cv::Mat &image,
-	std::vector<cv::KeyPoint> &key_pts,
-	cv::Mat &descriptors
+    cv::Mat &image,
+    std::vector<cv::KeyPoint> &key_pts,
+    cv::Mat &descriptors
 )
 {
-	this->fextractor->compute(image, key_pts, descriptors);
-	return 0;
+    this->fextractor->compute(image, key_pts, descriptors);
+    return 0;
 }
 
 int Vision::matchFeaturesBF(cv::Mat d1, cv::Mat d2)
 {
-	float ratio;
-	std::vector<cv::DMatch> good_matches;
-	std::vector< std::vector<cv::DMatch> > matches;
+    float ratio;
+    std::vector<cv::DMatch> good_matches;
+    std::vector< std::vector<cv::DMatch> > matches;
 
-	// setup
-	ratio = 0.8;
+    // setup
+    ratio = 0.8;
 
-	// match
-	this->fmatcher->knnMatch(d1, d2, matches, 2);
+    // match
+    this->fmatcher->knnMatch(d1, d2, matches, 2);
 
-	// ratio test as in Lowe's paper; can be tuned
-	for (size_t i = 0; i < matches.size(); i++) {
-		if (matches[i][0].distance < ratio * matches[i][1].distance) {
-			good_matches.push_back(matches[i][0]);
-		}
+    // ratio test as in Lowe's paper; can be tuned
+    for (size_t i = 0; i < matches.size(); i++) {
+        if (matches[i][0].distance < ratio * matches[i][1].distance) {
+            good_matches.push_back(matches[i][0]);
+        }
 
-		if (good_matches.size() > 10) {
-			break;
-		}
-	}
+        if (good_matches.size() > 10) {
+            break;
+        }
+    }
 
-	return 0;
+    return 0;
 }
 
 int Vision::matchFeaturesFlann(cv::Mat d1, cv::Mat d2)
 {
-	double dist;
-	double max_dist;
-	double min_dist;
-	std::vector<cv::DMatch> matches;
-	std::vector<cv::DMatch> good_matches;
+    double dist;
+    double max_dist;
+    double min_dist;
+    std::vector<cv::DMatch> matches;
+    std::vector<cv::DMatch> good_matches;
 
-	// setup
-	dist = 0.0;
-	max_dist = 0.0;
-	min_dist = 100.0;
+    // setup
+    dist = 0.0;
+    max_dist = 0.0;
+    min_dist = 100.0;
 
-	// match
-	this->fmatcher->match(d1, d2, matches);
+    // match
+    this->fmatcher->match(d1, d2, matches);
 
-	// find min max distances between keypoints
-	for (int i = 0; i < d1.rows; i++) {
-		dist = matches[i].distance;
-		min_dist = (dist < min_dist) ? dist : min_dist;
-		max_dist = (dist > max_dist) ? dist : max_dist;
-	}
+    // find min max distances between keypoints
+    for (int i = 0; i < d1.rows; i++) {
+        dist = matches[i].distance;
+        min_dist = (dist < min_dist) ? dist : min_dist;
+        max_dist = (dist > max_dist) ? dist : max_dist;
+    }
 
-	// filter only good matches
-	for (int i = 0; i < d1.rows; i++) {
-		if (matches[i].distance <= std::max(2 * min_dist, 0.05)) {
-			good_matches.push_back(matches[i]);
-		}
-	}
+    // filter only good matches
+    for (int i = 0; i < d1.rows; i++) {
+        if (matches[i].distance <= std::max(2 * min_dist, 0.05)) {
+            good_matches.push_back(matches[i]);
+        }
+    }
 
-	return 0;
+    return 0;
 }
